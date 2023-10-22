cf8b7a079d64ee75d3923c36d3ffd77995ccd8dd
mira <miraslamaj@gmail.com>
2023-10-22 23:27:25 +0300

    sth

--- inverted_index.py
+++ inverted_index.py
@@ -1,9 +1,16 @@
 import os
 
-folder_path = 'docs'
-items = os.listdir(folder_path)
-
-# Count the number of files
+#in order to count the number of files in docs folder 
+#we know there are only txt files in that folder 
+items = os.listdir('docs')
 num_files = len(items)
 
-print(f'The folder "{folder_path}" contains {num_files} files.')
+terms=[] #initialization of empty list of terms in docs
+
+for item in items :
+    f = open(item, "r") #open every file in "docs" to read
+    tokens=f.split(); #Split the text into words using spaces as the delimiter
+    terms = list(set(terms+tokens))# Combine the tokens into a list of unique terms
+
+print(f'The folder docs contains {num_files} files.')
+print(terms)

e0420275979eec6c6ed7de24f6c07621b17bd4a4
mira <miraslamaj@gmail.com>
2023-10-25 22:44:17 +0300

    lexical analysis

--- inverted_index.py
+++ inverted_index.py
@@ -1,16 +1,20 @@
 import os
 
+path ='docs'
+os.chdir(path) #change directory so that we can see files inside of 'docs' 
 #in order to count the number of files in docs folder 
 #we know there are only txt files in that folder 
-items = os.listdir('docs')
+items = os.listdir()
 num_files = len(items)
 
 terms=[] #initialization of empty list of terms in docs
 
 for item in items :
-    f = open(item, "r") #open every file in "docs" to read
-    tokens=f.split(); #Split the text into words using spaces as the delimiter
-    terms = list(set(terms+tokens))# Combine the tokens into a list of unique terms
+    with open(item, "r") as f: #open every file in "docs" to read
+        text=f.read()
+        tokens=text.split(); #Split the text into words using spaces as the delimiter
+        terms = list(set(terms+tokens))# Combine the tokens into a list of unique terms
 
-print(f'The folder docs contains {num_files} files.')
-print(terms)
+with open("words.txt", "w+") as file:
+    for term in terms :
+        file.write("%s\n" % term)

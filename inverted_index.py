c55c380825d626251291cfe1192cff91c61ce119
mira <miraslamaj@gmail.com>
2023-10-31 17:19:56 +0200

    full inverted index

--- inverted_index.py
+++ inverted_index.py
@@ -7,19 +7,20 @@ os.chdir(path) #change directory so that we can see files inside of 'docs'
 #we know there are only txt files in that folder 
 docs = os.listdir()
 
-data = []  # Create a list to store dictionaries for each term
-terms = set()  # Use a set to store unique terms
+data = []  # Create a list to store dictionaries for each word
+words = []  # Use a set to store unique words
 
 for doc_id in docs:
     with open(doc_id, "r") as f:
         text = f.read()
         tokens = text.split()
-        terms.update(tokens)  # Add terms to the set
+        words=set(tokens)  # Add words to the set
 
-        for term in set(tokens):  # Use a set to get unique terms per document
-            term_frequency = tokens.count(term)
-            document_info = [doc_id, term_frequency]
-            data.append({'Term': term, 'DocumentInfo': document_info})
+        for word in set(tokens):  # Use a set to get unique words per document
+            word_frequency = tokens.count(word)
+            positions = [index for index, token in enumerate(tokens,start=1) if token == word]
+            document_info = [doc_id, word_frequency,positions]
+            data.append({'word': word, 'DocumentInfo': document_info})
 
 os.chdir('..') #so that we are in the main directory of the project
 #every word used in the C.F. collection is stored at lexicon.txt file
@@ -27,7 +28,7 @@ os.chdir('..') #so that we are in the main directory of the project
 inverted_index = pd.DataFrame(data)
 
 # Group and aggregate the DocumentInfo column
-inverted_index = inverted_index.groupby('Term')['DocumentInfo'].apply(list).reset_index()
+inverted_index = inverted_index.groupby('word')['DocumentInfo'].apply(list).reset_index()
 
 # Save the DataFrame to a CSV file
 inverted_index.to_csv('inverted_index.csv', index=False)

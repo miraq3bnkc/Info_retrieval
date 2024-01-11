0d67bd606f326dc9593d611f7c157004a6be5540
mira <miraslamaj@gmail.com>
2024-01-11 22:57:32 +0200

    file correction

--- change_format_ranking.py
+++ change_format_ranking.py
@@ -1,25 +1,9 @@
 import os
 import pandas as pd 
-
-def insert_header(file_path, header):
-    # Read the existing content of the file
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-
-    # Insert the new header line at the beginning of the content
-    lines.insert(0, header)
-
-    # Write the updated content back to the file
-    with open(file_path, 'w') as file:
-        file.writelines(lines)
-
   
 def change(filename):
     # read contents of tsv file 
     file = pd.read_csv(filename, delimiter='\t', header=None) 
-    # adding header 
-    headerLine = 'Query\tDocument_ID\tRank\tSimilarity\n' 
-    insert_header(filename,headerLine)
 
      # Get a list of filenames in the 'docs/' directory
     docs_filenames = sorted(os.listdir('docs/'))
@@ -27,18 +11,31 @@ def change(filename):
     # Replace 'Document_ID' values with corresponding filenames
     file.iloc[:,1] = [docs_filenames[i] for i in file.iloc[:,1]]
 
+    # Create a new DataFrame with the header and updated column values
+    new_file = pd.DataFrame(file.values, columns=['Query', 'Document_ID', 'Rank', 'Similarity'])
+
     # converting data frame to tsv and updating the existing file
-    file.to_csv(filename, sep='\t', index=False, mode='a')  
+    new_file.to_csv(filename, sep='\t', index=False)  
   
-# Example usage:
-change("/home/mira/Documents/uni/11th/anaktisi/Info_retrieval/experiments/notebook/using_colbert/2024-01/06/20.14.26/ranking.tsv")
+
+def has_expected_header(filename):
+    # Read the first line of the file
+    with open(filename, 'r') as file:
+        first_line = file.readline()
+
+    # Check if the first line matches the expected header
+    expected_header = 'Query\tDocument_ID\tRank\tSimilarity\n'
+    return first_line == expected_header
 
 
 def find_files(filename):
    # Walking top-down from the root
    for root, dir, files in os.walk('experiments/notebook/using_colbert'):
       if filename in files:
-         name=os.path.join(root, filename)
-         change(name)
+        full_path=os.path.join(root, filename)
+        if not has_expected_header(full_path):
+            change(full_path)
+            print ("sdgvj")
+        return full_path
 
 find_files('ranking.tsv')

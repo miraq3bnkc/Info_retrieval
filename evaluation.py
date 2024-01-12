20358f14e4013e48a423ca2e26107e9aeb0da489
mira <miraslamaj@gmail.com>
2024-01-13 01:24:36 +0200

    ndcg unfinished

--- evaluation.py
+++ evaluation.py
@@ -11,28 +11,42 @@ For each method we ranked k=100 similar documents for every query.
 
 import pandas as pd
 from find_and_change_ranking import find_files
+import matplotlib.pyplot as plt
+import numpy as np
 
-#RECALL FUNCTION 
+#RECALL@K FUNCTION
 def recall(retrieved,relevant,k):
-    x=len(relevant.intersection(retrieved[:k]))
+    x=len(set(relevant).intersection(retrieved[:k]))
     y=len(relevant)
     return x/y
 
+#PRECISION@K FUNCTION
 def precision(retrieved,relevant,k):
     x=len(set(relevant).intersection(retrieved[:k]))
     return x/k
 
-def average_precision(K,retrieved,relevant):
-    sum=0
-    for k in range(K):
-        if retrieved[k] in relevant:
-            rel=1
-        else:
-            rel=0
-        mul=precision(retrieved,relevant,k)*rel
-        sum= sum + mul
-    AP=sum / len(relevant)
-    return AP
+def recall_precision_graph():
+    #lets just look for it on the book later
+    return 0
+
+#CALCULATION OF MAP (MEAN AVERAGE PRECISION) EVALUATION METRIC
+def mean_average_precision(K,retrieved,relevant):
+    big_sum=0
+    for query in range(20):
+        predicted=retrieved[query]
+        actual=relevant[query]
+        sum=0
+        for k in range(K):
+            if predicted[k] in actual:
+                rel=1
+            else:
+                rel=0
+            mul=precision(predicted,actual,k+1)*rel
+            sum= sum + mul
+        AP=sum / len(relevant)
+        big_sum=big_sum+AP
+    mean_average_precision=big_sum/20
+    return mean_average_precision
 
 def get_retrieved_docs(dataframe):
     # Create an empty dictionary to store results
@@ -42,7 +56,7 @@ def get_retrieved_docs(dataframe):
     for query in range(20):
 
         # Filter rows where "Query" column is equal to the current query
-        filtered_df = dataframe[dataframe['Query'] == query]
+        filtered_df = dataframe[dataframe['Query'] == query+1]
 
         # Extract values from "Document_ID" column and store in a list
         document_ids_list = filtered_df['Document_ID'].tolist()
@@ -51,6 +65,7 @@ def get_retrieved_docs(dataframe):
         result_list.append(document_ids_list)
     return result_list
 
+
 colbert_rank=pd.read_csv(find_files("ranking.tsv"),delimiter='\t')
 vsm_rank=pd.read_csv("vsm_rankings.csv")
 
@@ -58,13 +73,15 @@ vsm_rank=pd.read_csv("vsm_rankings.csv")
 with open("Relevant_20") as f:
     queries_relevant=f.readlines()
 #list of relevant documents in a list for each query (list of lists)
-relevant = [line.strip().split() for line in queries_relevant]
+relevant = [list(map(int,line.strip().split())) for line in queries_relevant] #every doc name is stored as int
+
 
 #list of retrieved documents for each query (list of lists)
 retrieved_colbert=get_retrieved_docs(colbert_rank)
 retrieved_vsm=get_retrieved_docs(vsm_rank)
 
-for query in range(3):
-    x=average_precision(100,retrieved_colbert[query],relevant[query])
-    print(x)
-    print('......................................................................................')
+map_colbert=mean_average_precision(100,retrieved_colbert,relevant)
+print(map_colbert)
+
+map_vsm=mean_average_precision(100,retrieved_vsm,relevant)
+print(map_vsm)

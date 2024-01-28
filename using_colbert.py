af86004f51e5b70b397b91f80f055fde31da83bb
mira <miraslamaj@gmail.com>
2024-01-28 21:37:09 +0200

    colbert update

--- using_colbert.py
+++ using_colbert.py
@@ -9,7 +9,6 @@ from colbert import Searcher
 docs_directory = 'docs/' #path to your directory containing the documents
 
 collection = []  #List containing 1209 documents
-line_counts=[] #it stores how many lines each doc has
 queries=[]
 
 # Iterate through all files in the directory
@@ -20,15 +19,6 @@ for filename in sorted(os.listdir(docs_directory)):
         # Read the contents of the file and append it to the collection
         content = file.read()
         collection.append(content)
-
-        #calculate number of lines and store them to line_counts list
-        lines=content.split('\n')  # Split content into lines
-        line_counts.append(len(lines))
-
-# Calculate the most common number of lines among the documents
-common_line_count = Counter(line_counts).most_common(1)
-doc_maxlen = common_line_count[0][0] if common_line_count else 300  # Default to 300 if no line count data
-#doc_maxlen truncates docs at a number of tokens
 
 # Read the queries from the file into a list
 with open('Queries_20', 'r') as file:
@@ -48,7 +38,7 @@ if __name__=='__main__':
 
         # Indexing
     with Run().context(RunConfig(nranks=1, experiment='notebook')):
-        config = ColBERTConfig(doc_maxlen=doc_maxlen, nbits=2)
+        config = ColBERTConfig( nbits=2)
 
         indexer = Indexer(checkpoint=checkpoint, config=config)
         indexer.index(name=index_name, collection=collection[:max_id], overwrite=True)
